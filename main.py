491acff975b9636454ce5844b4c0d21cd42fd65b
Trần Thị Tứ Linh <tranthitulinh1305@gmail.com>
2023-05-19 10:40:53 +0700

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ appTest = FastAPI() # gọi constructor và gán vào biến app  // tạo 1 ins
 
 @appTest.get("/") # giống flask, khai báo phương thức get và url // tạo đường dẫn, bắt đầu từ / // khai báo phương thức HTTP: post, get, put, delete hay options, head, patch, trace
 async def test(): # do dùng ASGI nên ở đây thêm async, nếu bên thứ 3 không hỗ trợ thì bỏ async đi // khai báo hàm
-    return {"message": "Hello Worldd"} # // trả về content với format dict, list, str, int, ...
+    return {"message": "Hello Worldd 2"} # // trả về content với format dict, list, str, int, ...
 
 
 @appTest.get("/items/{item_id}") # khai báo đường dẫn có tham số // khai báo đường dẫn có tham số   
