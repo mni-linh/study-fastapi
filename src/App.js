d3b6b42bfb02b2d382352b92483bccec124b8b6c
Trần Thị Tứ Linh <tranthitulinh1305@gmail.com>
2023-05-19 10:58:35 +0700

    update get api

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import axios from "axios";
+import { useEffect } from "react";
+import { useState } from "react";
 
 function App() {
+  const [products, setProducts] = useState([]); // [
+  // const response = fetch("https://hama-be.vercel.app/api/products")
+  //   .then((res) => res.json())
+  //   .then((res) => {
+  //     console.log(res);
+  //   });
+
+  // const getProducts = async () => {
+  //   const response = await axios.get("https://hama-be.vercel.app/api/products");
+  //   setProducts(response.data);
+  //   console.log(response);
+  // };
+
+  const getProducts = () => {
+    axios.get("https://hama-be.vercel.app/api/products").then((res) => {
+      setProducts(res.data);
+    });
+  };
+
+  useEffect(() => {
+    getProducts();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {products.map((product) => (
+        <div key={product.id}>
+          <img src={product.images[0]} alt="" />
+          <h1>{product.name}</h1>
+          <p>{product.price}</p>
+        </div>
+      ))}
     </div>
   );
 }
